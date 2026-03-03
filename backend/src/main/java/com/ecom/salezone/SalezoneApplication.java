baed5029016b8b1158f2b2ff02c8f9145594b65a
sandeepswain12 <sandeepswain027@gmail.com>
2026-03-04 00:16:11 +0530

    if role not exist then added at time of application start

--- backend/src/main/java/com/ecom/salezone/SalezoneApplication.java
+++ backend/src/main/java/com/ecom/salezone/SalezoneApplication.java
@@ -1,13 +1,45 @@
 package com.ecom.salezone;
 
+import com.ecom.salezone.enities.Role;
+import com.ecom.salezone.repository.RoleRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class SalezoneApplication {
+public class SalezoneApplication implements CommandLineRunner {
 
-	public static void main(String[] args) {
-		SpringApplication.run(SalezoneApplication.class, args);
-	}
+    @Autowired
+    private RoleRepository roleRepository;
 
+    public static void main(String[] args) {
+        SpringApplication.run(SalezoneApplication.class, args);
+    }
+
+    @Override
+    public void run(String... args) {
+
+        // ADMIN ROLE
+        roleRepository.findById("ROLE_ADMIN")
+                .ifPresentOrElse(
+                        role -> System.out.println("Admin Role Already Exists"),
+                        () -> {
+                            Role role = new Role("ROLE_ADMIN", "ADMIN");
+                            roleRepository.save(role);
+                            System.out.println("Admin Role Created");
+                        }
+                );
+
+        // USER ROLE
+        roleRepository.findById("ROLE_USER")
+                .ifPresentOrElse(
+                        role -> System.out.println("User Role Already Exists"),
+                        () -> {
+                            Role role = new Role("ROLE_USER", "USER");
+                            roleRepository.save(role);
+                            System.out.println("User Role Created");
+                        }
+                );
+    }
 }
