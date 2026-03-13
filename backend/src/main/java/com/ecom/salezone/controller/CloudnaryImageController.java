541bae2b7551d981ad1cfa97c542a12ce63920a9
sandeepswain12 <sandeepswain027@gmail.com>
2026-03-13 22:58:30 +0530

    planning to implement cloudnary storage for images

---
+++ backend/src/main/java/com/ecom/salezone/controller/CloudnaryImageController.java
@@ -0,0 +1,4 @@
+package com.ecom.salezone.controller;
+
+public class CloudnaryImageController {
+}
---
+++ backend/src/main/java/com/ecom/salezone/services/CloudnaryImageService.java
@@ -0,0 +1,4 @@
+package com.ecom.salezone.services;
+
+public interface CloudnaryImageService {
+}
